efa18d3b5f90813c29a1c50f8da537dcab0711fe
dimaliwatkent <dimaliwatkent@gmail.com>
2025-02-16 18:58:00 +0800

    add cors origin

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+  "https://159.223.95.246:3010",
+  "http://159.223.95.246:3010",
+];
+
+module.exports = allowedOrigins;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 3020;
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "dist")));
 
