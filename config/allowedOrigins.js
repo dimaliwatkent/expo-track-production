590bd12478d42511c684e25c2b8cf3200a45d456
dimaliwatkent <dimaliwatkent@gmail.com>
2025-02-16 19:45:01 +0800

    .

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,3 @@
-const allowedOrigins = [
-  "https://159.223.95.246:3010",
-  "http://159.223.95.246:3010",
-];
+const allowedOrigins = ["http://159.223.95.246:3010"];
 
 module.exports = allowedOrigins;
